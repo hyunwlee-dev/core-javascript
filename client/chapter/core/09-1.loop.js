bd8050ec5400adff721fb3da218b72a4b467fe92
hyunwlee-dev <hwlee3928@gmail.com>
2023-01-03 09:33:45 +0900

    [추가] Object.entries 학습

---
+++ client/chapter/core/09-1.loop.js
@@ -0,0 +1,58 @@
+/* ---------------------------------------------------------------------- */
+/* While Loop                                                             */
+/* ---------------------------------------------------------------------- */
+
+// let i = 0;
+
+// while (i < 10) {
+  // console.log('안녕');
+  // ++i;
+// }
+
+
+
+const frontEndDev = [
+  'HTML',
+  'CSS',
+  'SVG',
+  'JavaScript',
+  'jQuery',
+  'React',
+  'Redux',
+];
+let z1 = performance.now();
+let i = 0;
+while (i < frontEndDev.length) {
+  console.log(frontEndDev[i++]);
+}
+
+let z2 = performance.now();
+i = frontEndDev.length;
+while (--i >= 0) {
+  console.log(frontEndDev[i]);
+}
+
+console.log(z2 - z1);
+
+let copyArray = [...frontEndDev];
+
+while (copyArray.length) {
+  console.log(copyArray.shift());
+}
+/* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
+
+// console.log(frontEndDev[0]);
+// console.log(frontEndDev[1]);
+// console.log(frontEndDev[2]);
+// console.log(frontEndDev[3]);
+// console.log(frontEndDev[4]);
+// console.log(frontEndDev[5]);
+
+
+/* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
+
+// while 문 (순환 : 순방향)
+
+// while 문 (역순환 : 역방향)
+
+// 성능 진단 : 순환 vs. 역순환
